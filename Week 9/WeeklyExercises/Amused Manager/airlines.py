5418de87e9b60289b18df2a855e520fda04afc5f
Runi <Runiniclasen@hotmail.com>
2020-03-30 23:53:06 +0200

    w9extra-postCLEANUP

--- Week 9/WeeklyExercises/Amused Manager/airlines.py
+++ Week 9/WeeklyExercises/Amused Manager/airlines.py
@@ -22,40 +22,19 @@ def exercise1():
     pattern = re.compile(r'([A-Z]{3}),')
     airports = set()  # distinct list?
     edges = []
-    #result_list = []
     with open('routes.dat', 'r', encoding='utf-8') as file_object:
         for line in file_object:
             result = re.findall(pattern, line)
             airport = result[0]
             edge = result[1]
-
+            # add to airport list, add to edge list
             airports.add(airport)
             edges.append((airport, edge))
-            #print(airport, edge)
-    #         if (airport in result_list):
-    #             print('here')
-    #             result_list[airport].append([edge])
-    #         else:
-    #             # print('here2')
-    #             data = list((airport, edge))
-    #             # print(data)
-    #             result_list.append(data)  # better way?
-    #         # print(line)
-    #         # print(result)
-    #         # airports.append(result[0])
-    #         # edges.append(result[1])
-    #         # result_list.append(result[0])
-    #         # result_list.append(result[1])
-    # #result_list = []
-    # # for airport, edge in zip(airports, edges):
-    #     #result_list.extend([(airport, edge)])
     return airports, edges
 
 
 airports, edges = exercise1()
-#print('here', result)
 result_np = np.array(edges)
-print(edges[:10])
 print(result_np)
 
 
@@ -84,10 +63,10 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join('static', 'img')
 
 
-def exercise3():
-    """
-    Create a flask server that can show this graph
-    """
+# exercise3():
+# """
+# Create a flask server that can show this graph
+# """
 
 
 @app.route('/', methods=['GET'])
@@ -104,8 +83,6 @@ def show_graph():
     </html>
     """  # lazy
     path = os.path.join(app.config['UPLOAD_FOLDER'], 'visualization.png')
-    print(path)
-    # only works locally :^)
     return render_template_string(index, image=path)
 
 
